cbd9b6ac016d640ebe68b2edd597713f5e2a0d2e
Muhammad Awais <awaiszahid684@gmail.com>
2025-12-24 23:12:21 +0500

    Create S3 bucket resource with versioning
    Added an AWS S3 bucket resource with versioning and public access restrictions.

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name-12345"  # change this to a unique name
+  acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+
+  # Block public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 data "aws_secretsmanager_secret_version" "database_password" {
   secret_id = "my-database-password-johndoe"
